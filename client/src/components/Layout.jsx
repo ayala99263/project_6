fcde0af3774f7b33ac3f76e429680e89ab40fddc
Tehila <your_email@example.com>
2026-01-11 22:33:54 +0200

    hi

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -6,6 +6,7 @@ import Navbar from './Navbar';
 const Layout = () => {
     const navigate = useNavigate();
     const user = JSON.parse(localStorage.getItem('currentUser'));
+    const [showInfoState, setShowInfoState] = React.useState(false);
 
     useEffect(() => {
         if (!user) {
@@ -19,16 +20,36 @@ const Layout = () => {
         navigate('/');
     };
 
+    const setShowInfo = () => {
+        setShowInfoState(true)
+    };
+
     if (!user) return null;
 
     return (
-        <div className="app-layout">
-            <Navbar user={user} handleLogout={handleLogout} />
-            
-            <main style={{ padding: '20px' }}>
-                <Outlet /> 
-            </main>
-        </div>
+        <>
+            <div className="app-layout">
+                <Navbar user={user} handleLogout={handleLogout} setShowInfo={setShowInfo} />
+                {showInfoState && <div>
+                    <button onClick={() => setShowInfoState(false)}>X</button>
+                    <h2>Info</h2>
+                    <p>Name: {user.name}</p>
+                    <p>Email: {user.email}</p>
+                    <p>Phone: {user.phone}</p>
+                    <p>address:</p>
+                    <p>street: {user.address.street}</p>
+                    <p>city: {user.address.city}</p>
+                    <p>number: {user.address.number}</p>
+                    <p>company:</p>
+                    <p>name: {user.company.name}</p>
+                    <p>catchPhrase: {user.company.catchPhrase}</p>
+                    <p>bs: {user.company.bs}</p>
+                </div>}
+                <main style={{ padding: '20px' }}>
+                    <Outlet />
+                </main>
+            </div>
+        </>
     );
 };
 
